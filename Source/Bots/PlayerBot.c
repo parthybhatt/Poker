2ec442ef0580c6239c2d5802ecf0671dc91a65bb
parthybhatt <parthybhatt@users.noreply.github.com>
2022-12-06 23:06:03 -0600

    Added code to PlayerBot module
    Player bots can hold cards

---
+++ Source/Bots/PlayerBot.c
@@ -0,0 +1,79 @@
+/********************************************************************************
+* Filename    : PlayerBot.h
+
+* Author      : Parth Bhatt
+
+* Date        : December 6, 2022
+
+* Description : Creates and handles the bots that will be playing poker with
+* 			    each other.
+
+********************************************************************************/
+
+/*******************************************************************************
+* Include Files
+********************************************************************************/
+#include <stdbool.h>
+#include <stdint.h>
+#include "card_deck.h"
+#include "PlayerBot.h"
+
+/*******************************************************************************
+* Constants and macros
+********************************************************************************/
+
+/*******************************************************************************
+* Data types (Structs, enum)
+********************************************************************************/
+
+/*******************************************************************************
+* Static Variables
+********************************************************************************/
+
+/*******************************************************************************
+* Static Functions Declaration
+********************************************************************************/
+
+/*******************************************************************************
+* Public Functions
+********************************************************************************/
+
+void PlayerBot_Init(PlayerBot_t* playerBot)
+{
+	PlayerBot_ResetCards(playerBot);
+}
+
+void PlayerBot_ResetCards(PlayerBot_t* playerBot)
+{
+	for(uint8_t i = 0; i < MAX_CARDS_PER_PLAYER; i++)
+	{
+		playerBot->cards[i].Number = 0xFF;
+		playerBot->cards[i].Shape = 0xFF;
+	}
+	playerBot->numCardsServed = 0;
+	playerBot->changesToWin = 0;
+}
+
+bool PlayerBot_ServeCardToBot(PlayerBot_t* playerBot, CardType_t card)
+{
+	if( playerBot->numCardsServed < MAX_CARDS_PER_PLAYER)
+	{
+		playerBot->cards[playerBot->numCardsServed] = card;
+		playerBot->numCardsServed++;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void PlayerBot_Think(PlayerBot_t* playerBot)
+{
+	//
+}
+
+/*******************************************************************************
+* Static Functions
+********************************************************************************/
+
--- Source/Bots/PlayerBot.h
+++ Source/Bots/PlayerBot.h
@@ -1,3 +1,50 @@
+/********************************************************************************
+* Filename    : PlayerBot.h
 
+* Author      : Parth Bhatt
 
-bool CreateInstance();
+* Date        : December 6, 2022
+
+* Description : Creates and handles the bots that will be playing poker with
+* 			    each other.
+
+********************************************************************************/
+
+#ifndef __PLAYER_BOT_H__
+#define __PLAYER_BOT_H__
+
+/*******************************************************************************
+* Include Files
+********************************************************************************/
+#include <stdbool.h>
+#include <stdint.h>
+#include "card_deck.h"
+
+/*******************************************************************************
+* Constants and macros
+********************************************************************************/
+#define MAX_CARDS_PER_PLAYER	(2)
+
+/*******************************************************************************
+* Data types
+********************************************************************************/
+typedef struct{
+	CardType_t cards[MAX_CARDS_PER_PLAYER];
+	uint8_t    numCardsServed;
+	uint8_t	   changesToWin;
+} PlayerBot_t;
+
+/*******************************************************************************
+* Global Variables
+********************************************************************************/
+
+/*******************************************************************************
+* Functions
+********************************************************************************/
+
+void PlayerBot_Init(PlayerBot_t* playerBot);
+void PlayerBot_ResetCards(PlayerBot_t* playerBot);
+bool PlayerBot_ServeCardToBot(PlayerBot_t* playerBot, CardType_t* card);
+void PlayerBot_Think(PlayerBot_t* playerBot);
+
+#endif
